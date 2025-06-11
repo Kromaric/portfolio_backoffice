7c5e1dbc1738b8401a3f1b28310c7bd7e890f01b
Kromaric <romaricyt11@gmail.com>
2025-06-11 12:25:57 +0200

    add skill

---
+++ app/Filament/Resources/SkillResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SkillResource\Pages;
+use App\Filament\Resources\SkillResource\RelationManagers;
+use App\Models\Skill;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SkillResource extends Resource
+{
+    protected static ?string $model = Skill::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->label('Skill Name')
+                    ->unique(ignoreRecord: true),
+
+                Forms\Components\TextInput::make('percentage')
+                    ->required()
+                    ->numeric()
+                    ->minValue(0)
+                    ->maxValue(100)
+                    ->default(0)
+                    ->label('Skill Percentage'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->searchable(),
+                Tables\Columns\TextColumn::make('percentage')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSkills::route('/'),
+            'create' => Pages\CreateSkill::route('/create'),
+            'edit' => Pages\EditSkill::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ config/filament.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'assets' => [
+        'css/filament/admin/theme.css',
+
+    ],
+];
