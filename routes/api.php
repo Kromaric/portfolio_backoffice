8c637fc6608f6c3f532c25fcd0d5652407e31b90
Kromaric <romaricyt11@gmail.com>
2025-06-12 00:06:39 +0200

    add endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TechnologyController;
+use App\Http\Controllers\Api\SkillController;
+use App\Http\Controllers\Api\ClientController;
+use App\Http\Controllers\Api\TestimonialController;
+use App\Http\Controllers\Api\BlogPostController;
+use App\Http\Controllers\Api\ContactController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +26,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/skills', [SkillController::class, 'index']);
+Route::get('/technologies', [TechnologyController::class, 'index']);
+Route::get('/clients', [ClientController::class, 'index']);
+Route::get('/testimonials', [TestimonialController::class, 'index']);
+Route::get('/blog-posts', [BlogPostController::class, 'index']);
+Route::get('/blog-posts/{id}', [BlogPostController::class, 'show']);
+Route::post('/contacts', [ContactController::class, 'store']);
